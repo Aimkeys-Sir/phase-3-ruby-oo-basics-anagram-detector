661d3677cf0d784fb7cf64b4e084e93e4b4660e0
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-08-18 18:36:17 +0300

    commit tree 5c0b121e9b3e5ff0d7388478635bd8f32869ffb4

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word=word
+    end
+
+    def match(possible_matches)
+        possible_matches.filter do |p_match|
+          p_match.split("").sort==@word.split("").sort
+        end
+    end
+end
+  diaper=Anagram.new("diaper")
+ pp diaper.match(%w[hello world zombies pants dipper])
